d303d95b6daab2c0cee3201033ad5d809a80a240
Gohil Vishal <91748441+Gohilvd04@users.noreply.github.com>
2022-08-28 17:54:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let charIndex = 0;
 function updateText() {
     charIndex++;
     containerEl.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1) === "I" ?"an":"a" } ${careers[careerIndex].slice(0,charIndex)}</h1>`;    
-    
+    //${careers[careerIndex].slice(0,1) === "" ?"an":"a" } if u were use vowels in your name array
     setTimeout(updateText,200);
         if (charIndex === careers[careerIndex].length) {
             careerIndex++;
